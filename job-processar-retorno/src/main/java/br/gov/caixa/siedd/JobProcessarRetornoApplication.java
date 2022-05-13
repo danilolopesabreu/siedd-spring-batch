27db0a80cfb67f366cee79baf0faeffbbdfda557
danilolopesabreu <danilo.bcc@gmail.com>
2022-05-12 23:10:37 -0300

    add spring batch

---
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/JobProcessarRetornoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.siedd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobProcessarRetornoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobProcessarRetornoApplication.class, args);
+	}
+
+}
---
+++ job-processar-retorno/src/test/java/br/gov/caixa/siedd/JobProcessarRetornoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.caixa.siedd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobProcessarRetornoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
