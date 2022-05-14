bea4ee57317431e2e528a3a140081f3e1ec25930
danilolopesabreu <danilo.bcc@gmail.com>
2022-05-14 15:34:42 -0300

    + layouts

---
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiaute/d05fduni/D05FDV01.java
@@ -0,0 +1,8 @@
+package br.gov.caixa.siedd.domain.leiaute.d05fduni;
+
+/**
+ * D05FDV01 - Pagamento de Cheque COMPE
+ * */
+public class D05FDV01 implements AreaVariavel {
+
+}
---
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiaute/d05fduni/D05FDV02.java
@@ -0,0 +1,8 @@
+package br.gov.caixa.siedd.domain.leiaute.d05fduni;
+
+/**
+ * D05FDV02 - Lançamento Retroativo
+ * */
+public class D05FDV02 implements AreaVariavel {
+
+}
