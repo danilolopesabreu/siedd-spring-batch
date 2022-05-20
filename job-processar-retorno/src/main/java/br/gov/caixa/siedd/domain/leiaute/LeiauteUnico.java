4bfbf78a4ee721c340ded1ea1790d56cbdf37b7d
danilolopesabreu <danilo.bcc@gmail.com>
2022-05-19 23:53:26 -0300

    remoção de referencias

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiaute/LeiauteUnico.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiaute/LeiauteUnico.java
@@ -9,8 +9,4 @@ import br.gov.caixa.siedd.domain.leiaute.d05fduni.D05fduniTrailler;
  * Classe que representa o layout: D05FDUNI – Remessa de Agendamentos/Lançamentos
  * */
 public class LeiauteUnico {
-	D05fduniHeader d05fduniHeader;
-	D05fduniAreaFixa areaFixa;
-	AreaVariavel areaVariavel;
-	D05fduniTrailler d05fduniTrailler;
 }
