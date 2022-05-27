127480a8c3bf2b64891f4d1d2f468df172264e7d
danilolopesabreu <danilo.bcc@gmail.com>
2022-05-26 23:22:11 -0300

    add header

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
@@ -4,5 +4,6 @@ package br.gov.caixa.siedd.domain.leiautes.d05fduni;
  * Classe que representa o layout: D05FDUNI – Remessa de Agendamentos/Lançamentos
  * */
 public class D05FDUNI {
-	//todo
+	private D05FDUNIHeader header;
+	private D05FDUNITrailler trailler;
 }
