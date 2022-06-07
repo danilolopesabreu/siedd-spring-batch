7c175c4f9f93fee2b5ff53ab9ac4dc42c0cbd149
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-06 22:52:20 -0300

    ajustes

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
@@ -2,8 +2,26 @@ package br.gov.caixa.siedd.domain.leiautes.d05fduni;
 
 /**
  * Classe que representa o layout: D05FDUNI – Remessa de Agendamentos/Lançamentos
- * */
+ */
 public class D05FDUNI {
+	
 	private D05FDUNIHeader header;
-	private D05FDUNITrailler trailler;//TODO
+	private D05FDUNITrailler trailler;
+
+	public D05FDUNIHeader getHeader() {
+		return header;
+	}
+
+	public void setHeader(D05FDUNIHeader header) {
+		this.header = header;
+	}
+
+	public D05FDUNITrailler getTrailler() {
+		return trailler;
+	}
+
+	public void setTrailler(D05FDUNITrailler trailler) {
+		this.trailler = trailler;
+	}
+
 }
