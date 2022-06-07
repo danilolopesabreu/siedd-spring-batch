5778000729990b2cf41abf5b5e145a83c1a93160
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-07 10:54:23 -0300

    testes de concatenação

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDUNI.java
@@ -1,9 +1,13 @@
 package br.gov.caixa.siedd.domain.leiautes.d05fduni;
 
+import org.springframework.batch.item.file.transform.Range;
+
+import br.gov.caixa.siedd.domain.util.SpringRange;
+
 /**
  * Classe que representa o layout: D05FDUNI – Remessa de Agendamentos/Lançamentos
  */
-public class D05FDUNI {
+public class D05FDUNI implements SpringRange {
 	
 	private D05FDUNIHeader header;
 	private D05FDUNITrailler trailler;
@@ -24,4 +28,23 @@ public class D05FDUNI {
 		this.trailler = trailler;
 	}
 
+	@Override
+	public Range[] getRange() {
+		return SpringRange.concatWithArrayCopy(
+						  this.header.getRange()
+						, this.trailler.getRange());
+	}
+	
+	public static void main(String[] args) {
+		Range[] arrayConcat = SpringRange.concatWithArrayCopy(
+					  new Range[]{ new Range(1,2),  new Range(3,4 )}
+					, new Range[]{ new Range(5,6),  new Range(7,8 )}
+  				    , new Range[]{ new Range(9,10),  new Range(11,12 )}
+					, new Range[]{ new Range(13,14),  new Range(15,16 )}  
+				);
+		
+		for (int i = 0; i < arrayConcat.length; i++) {
+			System.out.println(arrayConcat[i]);
+		}
+	}
 }
