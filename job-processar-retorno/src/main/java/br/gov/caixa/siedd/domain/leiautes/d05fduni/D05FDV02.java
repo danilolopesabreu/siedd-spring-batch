e84a9c709bb7c930d31e065616b5a284c982559d
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-08 16:07:02 -0300

    implementação springrange

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDV02.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDV02.java
@@ -1,8 +1,16 @@
 package br.gov.caixa.siedd.domain.leiautes.d05fduni;
 
+import org.springframework.batch.item.file.transform.Range;
+
 /**
  * D05FDV02 - Lançamento Retroativo
  * */
 public class D05FDV02 implements AreaVariavel {
+
+	@Override
+	public Range[] getRange() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
