2e08564819bf3c67fd500222ce6781a5fe3ff885
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-08 16:07:12 -0300

    implementação range

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDV01.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/D05FDV01.java
@@ -1,8 +1,16 @@
 package br.gov.caixa.siedd.domain.leiautes.d05fduni;
 
+import org.springframework.batch.item.file.transform.Range;
+
 /**
  * D05FDV01 - Pagamento de Cheque COMPE
  * */
 public class D05FDV01 implements AreaVariavel {
+
+	@Override
+	public Range[] getRange() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
