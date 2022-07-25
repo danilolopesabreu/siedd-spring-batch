23b90ad67579b68945914e443a57681ac1285abe
danilolopesabreu <danilo.bcc@gmail.com>
2022-07-24 21:36:57 -0300

    renomeando elementos

---
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/AreaFixa.java
@@ -0,0 +1,5 @@
+package br.gov.caixa.siedd.domain.leiautes.d05fduni;
+
+public class AreaFixa {
+
+}
--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/AreaVariavel.java
+++
@@ -1,7 +0,0 @@
-package br.gov.caixa.siedd.domain.leiautes.d05fduni;
-
-import br.gov.caixa.siedd.domain.util.SpringRange;
-
-public interface AreaVariavel extends SpringRange {
-
-}
