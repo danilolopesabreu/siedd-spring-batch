7903a6c1154d3f4cb4fe1bb4fc2050accc376d2d
danilolopesabreu <danilo.bcc@gmail.com>
2022-07-25 00:01:29 -0300

    definição do step

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/job/steps/ProcessarRetornoStepConfig.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/job/steps/ProcessarRetornoStepConfig.java
@@ -2,10 +2,15 @@ package br.gov.caixa.siedd.job.steps;
 
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import br.gov.caixa.siedd.domain.leiautes.d05fduni.D05FDUNI;
+import br.gov.caixa.siedd.job.steps.reader.ArquivoRetornoReader;
 
 @Configuration
 public class ProcessarRetornoStepConfig {
@@ -14,10 +19,16 @@ public class ProcessarRetornoStepConfig {
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
-	public Step stepProcessarRetorno(Tasklet testeTasklet) {
-		return this.stepBuilderFactory
+	public Step stepProcessarRetorno(
+			FlatFileItemReader<D05FDUNI> arquivoRetornoItemReader,
+			ItemProcessor<D05FDUNI, D05FDUNI> arquivoRetornoProcessor,
+			ItemWriter<D05FDUNI> arquivoRetornoWriter) {
+		return stepBuilderFactory
 				.get("stepProcessarRetorno")
-				.tasklet(testeTasklet)
+				.<D05FDUNI, D05FDUNI> chunk(20)
+				.reader(new ArquivoRetornoReader(arquivoRetornoItemReader))
+				.processor(arquivoRetornoProcessor)
+				.writer(arquivoRetornoWriter)
 				.build();
 	}
 }
