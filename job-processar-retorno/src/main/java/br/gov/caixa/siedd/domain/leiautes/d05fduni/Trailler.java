8ac3211c279b49d50f339db875712122f61b6123
Danilo R L Abreu <danilo.l.abreu@caixa.gov.br>
2022-07-26 15:22:06 -0300

    equals

--- job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/Trailler.java
+++ job-processar-retorno/src/main/java/br/gov/caixa/siedd/domain/leiautes/d05fduni/Trailler.java
@@ -1,5 +1,6 @@
 package br.gov.caixa.siedd.domain.leiautes.d05fduni;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -29,7 +30,7 @@ public class Trailler {
 	public static String[] getPropertiesName() {
 		final List<String> propriedades = Stream.of(new Trailler().getClass().getDeclaredFields())
 				.map(x -> x.getName() ).collect(Collectors.toList());
-		return (String[]) propriedades.toArray();
+		return Arrays.copyOf(propriedades.toArray(), propriedades.toArray().length, String[].class);
 	}
 
 	public short getTpRegistro() {
@@ -79,7 +80,52 @@ public class Trailler {
 	public void setFiller(String filler) {
 		this.filler = filler;
 	}
-	
-	
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((filler == null) ? 0 : filler.hashCode());
+		result = prime * result + (int) (qtRegistros ^ (qtRegistros >>> 32));
+		result = prime * result + (int) (qtTotalEfetivado ^ (qtTotalEfetivado >>> 32));
+		result = prime * result + tpRegistro;
+		result = prime * result + (int) (vrTotalEfetivado ^ (vrTotalEfetivado >>> 32));
+		result = prime * result + (int) (vrTotalRemessa ^ (vrTotalRemessa >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trailler other = (Trailler) obj;
+		if (filler == null) {
+			if (other.filler != null)
+				return false;
+		} else if (!filler.equals(other.filler))
+			return false;
+		if (qtRegistros != other.qtRegistros)
+			return false;
+		if (qtTotalEfetivado != other.qtTotalEfetivado)
+			return false;
+		if (tpRegistro != other.tpRegistro)
+			return false;
+		if (vrTotalEfetivado != other.vrTotalEfetivado)
+			return false;
+		if (vrTotalRemessa != other.vrTotalRemessa)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Trailler [tpRegistro=" + tpRegistro + ", qtRegistros=" + qtRegistros + ", vrTotalRemessa="
+				+ vrTotalRemessa + ", qtTotalEfetivado=" + qtTotalEfetivado + ", vrTotalEfetivado=" + vrTotalEfetivado
+				+ ", filler=" + filler + "]";
+	}
+	
 }
